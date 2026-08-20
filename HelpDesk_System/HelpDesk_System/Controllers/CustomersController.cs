3dcad463a47188c8a0bdd7aa7f177b9c4de6bf32
AlbanoRavenPOGI <onlyven711@gmail.com>
2026-08-20 11:14:43 -0700

    add CustomersController and Index view

---
+++ HelpDesk_System/HelpDesk_System/Controllers/CustomersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HelpDesk_System.Data;
+
+namespace HelpDesk_System.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CustomersController(AppDbContext context) => _context = context;
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Customers.ToListAsync());
+        }
+    }
+}
