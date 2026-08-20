b6af2da6a5a2a783f4b24882b5782d2c93852f2c
AlbanoRavenPOGI <onlyven711@gmail.com>
2026-08-20 11:03:39 -0700

    add DepartmentsController and Index view

---
+++ HelpDesk_System/HelpDesk_System/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HelpDesk_System.Data;
+
+namespace HelpDesk_System.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public DepartmentsController(AppDbContext context) => _context = context;
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Departments.ToListAsync());
+        }
+    }
+}
