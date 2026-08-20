715ddca0e4504d6377d39b128bd0a2a1240572dc
Althea Vallejos <teyakuromi.99@gmail.com>
2026-08-21 01:02:27 +0800

    add TicketComment and TicketHistory models

---
+++ HelpDesk_System/HelpDesk_System/Models/TicketComment.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Sockets;
+
+namespace HelpDesk_System.Models
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public int AuthorEmployeeId { get; set; }
+        public string CommentText { get; set; } = string.Empty;
+        public string CreatedAt { get; set; } = string.Empty;
+
+        public Ticket? Ticket { get; set; }
+        public Employee? Author { get; set; }
+    }
+}
